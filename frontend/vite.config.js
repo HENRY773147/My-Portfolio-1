760d9e41a08e0c84f07b475cb11c350efb1c5247
Dulanga Minoj Henry <minojhenry@gmail.com>
2025-08-20 22:00:11 +0530

    remove all changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: '/My-Portfolio-1/',
-   build: {
-    outDir: 'dist'
-   }
+  
 })
